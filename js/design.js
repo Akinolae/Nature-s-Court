1671712d38e5cc2c9a94639a2f0f1c728d218f6a
Akinolae <makindeakinola22@gmail.com>
2019-05-15 14:44:57 +0100

    New file

---
+++ js/design.js
@@ -0,0 +1,44 @@
+// let food = prompt('');
+// switch (food) {
+//   case 'crispy cheese':
+//     alert('delicious meal' || 'sweet tasting');
+//     break;
+//   case 'Eba yibo' :
+//     alert ('eri ibu ounje');
+//     break;
+//   case 'ikokore':
+//     alert('ounje aladun');
+//     break;
+//   case 'ofada rice':
+//     alert('good one');
+//     break;
+//   case 'french fries':
+//     alert('what the hell is that');
+//     break;
+//   case 'semovita':
+//     confirm('Are you okay at all?'); 
+//     break;
+//   default:
+//   alert('i guess you \'re a fruitarian');
+// // }
+// let car ={
+//   make: "Nissan",
+//   model: "Eseries007",
+//   year:  2098,
+//   getPrice: function () {
+//     return 5000;
+//   },
+//   printDescription: function (){
+//     console.log(this.make + ' ' + this.model);
+//   }
+// } 
+// car.printDescription();
+// font-family: New Century Schoolbook, TeX Gyre Schola, serif;
+
+//  li :hover{
+// 	padding: 10px;
+// 	background-color: rgba(241,237,237,0.7);
+// 	border-radius: 4px;
+// 	transform: scale(1.2);
+// 	transition: 0.5s;
+// }
---
+++ script.js
@@ -0,0 +1,6 @@
+4 * 3;
+
+
+if (4*3 === 12) {
+	alert("You're correct");
+}
